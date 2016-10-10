708762b5dff452808a71cde8291c07152dc419d6
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-10-10 20:45:54 +0200

    Refactor

--- Main.py
+++ Main.py
@@ -5,22 +5,21 @@ start = 1
 end = 1024
 address = "one.dev.parttrap.com"
 
-def testPort(add, prt):
+def testPort(ip, port):
     try:
         sock = socket.socket()
-        ip = socket.gethostbyname(add)
-        sock.connect((ip, prt))
-        print(str(prt) + " open")
+        sock.connect((ip, port))
+        print(str(port) + " open")
     except:
         pass
+
+ip = socket.gethostbyname(address)
 
 thread_list = []
 
 for port in range(start, end+1):
-    thread = threading.Thread(target=testPort, args=(address, port))
+    thread = threading.Thread(target=testPort, args=(ip, port))
     thread_list.append(thread)
-
-for thread in thread_list:
     thread.start()
 
 for thread in thread_list:
